0577949334480e30efbf73d93a0c7a5ca094f410
MRuhaib <theruhaibmuhammed@gmail.com>
2024-07-10 16:36:31 +0530

    labelled input sequences with their respective clades

---
+++ Model Evaluation/Clade Clustering/Data Labelling/read.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import biotite.sequence.align as biotiteAlign
+import biotite.sequence.io.fasta as fasta
+from biotite.sequence import NucleotideSequence
+
+metadata = pd.read_csv("metadata2.csv")
+
+
+def getUniqueClades():
+    clades = metadata["Clades"]
+    uniqueClades = []
+    distribution = []
+    for clade in clades:
+        if clade not in uniqueClades:
+            uniqueClades.append(clade)
+
+    for clade in uniqueClades:
+        count = 0
+        for seq in clades:
+            if seq == clade:
+                count += 1
+        distribution.append({"clade": clade, "count": count})
+
+    with open("clades.txt", "w+") as f:
+        f.write(str(distribution))
+
+
+def getSeqClades(file):
+    sequences = []
+    count = 1
+    fastaFile = fasta.FastaFile.read(file)
+    f = fasta.get_sequences(fastaFile)
+    for key, value in f.items():
+        name = key.split("_")
+        if len(name) == 4:
+            name = f"{name[0]}_{name[1]}"
+        else:
+            name = f"{name[0]}"
+        row = metadata.loc[metadata["Standardized name"] == name]
+        clade = row["Clades"].iloc[0]
+        sequences.append(
+            {"id": count, "name": name, "clade": clade.rstrip(" "), "seq": value}
+        )
+        count += 1
+
+    with open("labels.txt", "w+") as f:
+        f.write(str(sequences))
+
+
+getSeqClades("YAL001C.fasta")
